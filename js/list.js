6be67ed5e45d6585d57b8467b265fdf8193ba837
黄明 <862408684@qq.com>
2018-07-07 13:19:33 +0800

    张俊达完成优惠券

--- js/list.js
+++ js/list.js
@@ -1,53 +1,34 @@
-$(function () {
-    // 拿到id
-    var titleid = urlTool(window.location.href).titleid;
-    // console.log(titleid);
-    // 显示和隐藏分类
-    $(".searchbar").click(function (e) {
-        e.preventDefault();
-        $(".category").toggleClass("active");
-    });
-    $(".category .upmenu").click(function () {
-        $(".category").toggleClass("active");
+$(function(){
+    var id = urlTool(window.location.href).id;
+    // console.log(id);
 
-    });
-
-    // 分类渲染
     $.ajax({
-        url: 'http://mmb.ittun.com/api/getbaicaijiatitle',
-        type: 'get',
-        success: function (backData) {
-            console.log(backData);
-            var categoryHtml = template("categoriesid", backData);
-            $(".categories").html(categoryHtml);
-            var title=backData.result[titleid].title;
-            console.log(title);
-            $(".title .titles").html(title)
+        url:'http://mmb.ittun.com/api/getcouponproduct',
+        data:{couponid:id},
+        success:function(backdata){
+            // console.log(backdata);
+            var parhtml = template('par',backdata);
+            $('.particulars').html(parhtml);
+            
         }
-    });
+    })
 
-    function render(id) {
-        $.ajax({
-            url: 'http://mmb.ittun.com/api/getbaicaijiaproduct',
-            data: {
-                titleid: id
-            },
-            type: 'get',
-            success: function (backData) {
-                console.log(backData);
-                var popularhtml = template("popular", backData);
-                $(".details ul").html(popularhtml)
-            }
+    $('.mui-icon').click(function(){
+        back();
+    })
+
+
+      $('.particulars').on('click','li',function(){
+        //   alert('aaa');
+       var img = $(this).data('tu');
+       $('#galleryOverlay').html(img)
+        $('#galleryOverlay').css({
+            'display': 'block'
+        }).click(function(){
+            $('#galleryOverlay').css({
+                'display': 'none'
+            })
         })
-    }
-
-    render(titleid);
-
-    // 点击高亮
-  $(".tab li").click(function (e) { 
-      e.preventDefault();
-      $(this).addClass("active").siblings().removeClass("active");
-      render(titleid);
-      
-  });
+        $('#galleryOverlay').addClass('visible');
+      })
 })
